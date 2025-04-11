c301367ab1ab1f6e899817d8e2e7285c868ffbd3
rowanft <rowanft@gmail.com>
2025-04-10 21:25:10 -0400

    Update hotspots.js

--- hotspots.js
+++ hotspots.js
@@ -34,28 +34,4 @@ function goToRandom(tag) {
  *    Reads data-x1,y1,x2,y2 from each hotspot,
  *    waits for image to load, calculates scale, positions hotspots.
  */
-function setupHotspots({ originalWidth, originalHeight, imageId, hotspotSelector }) {
-  const img = document.getElementById(imageId);
-
-  img.addEventListener("load", () => {
-    const actualWidth = img.clientWidth;
-    const actualHeight = img.clientHeight;
-
-    const scaleX = actualWidth / originalWidth;
-    const scaleY = actualHeight / originalHeight;
-
-    // Position each hotspot
-    const hotspots = document.querySelectorAll(hotspotSelector);
-    hotspots.forEach(hotspot => {
-      const x1 = parseFloat(hotspot.dataset.x1);
-      const y1 = parseFloat(hotspot.dataset.y1);
-      const x2 = parseFloat(hotspot.dataset.x2);
-      const y2 = parseFloat(hotspot.dataset.y2);
-
-      hotspot.style.left = (x1 * scaleX) + "px";
-      hotspot.style.top = (y1 * scaleY) + "px";
-      hotspot.style.width = ((x2 - x1) * scaleX) + "px";
-      hotspot.style.height = ((y2 - y1) * scaleY) + "px";
-    });
-  });
-}
+/********************************************
